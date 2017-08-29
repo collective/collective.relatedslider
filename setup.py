9306de5ff218beace16d4f642853f185372a988d
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2017-08-28 20:30:06 -0500

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.relatedslider',
-    version='0.1',
+    version='0.2.dev0',
     description="Plone addon that creates a slider viewlet containing related content.",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
